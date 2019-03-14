b806147f5b544149241c659132fda1d30f1563a9
ines <iness.8@hotmail.es>
2019-03-14 20:30:43 +0100

    cliente

---
+++ cliente_chat.py
@@ -0,0 +1,37 @@
+import socket
+
+IP = "127.0.0.1"
+PORT = 8086
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(s)
+
+
+
+try:
+    s.connect((IP, PORT))
+    print("Conexión establecida con el servidor ")
+except OSError:
+    print("El Socket ya está usado")
+
+    s.close()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((IP, PORT))
+
+condition = True
+while condition:
+    out = input("Introduzca un mensaje: ")
+    outbox = str.encode(out)
+    s.send(outbox)
+    if out.lower() == "salir" :
+        print("saliendo")
+        condition = False
+    a = s.recv(2048).decode("utf-8")
+    print("El servidor nos dice: ", a)
+    if  a.lower() == "salir":
+        print("saliendo")
+        condition = False
+
+s.close()
+
